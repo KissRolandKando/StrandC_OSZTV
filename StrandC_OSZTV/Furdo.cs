805a87142185595aa74cdb3c0e873f1795546236
Roland Kiss <roland.kiss.kando@gmail.com>
2026-02-05 06:06:39 +0100

    Add project files.

---
+++ StrandC_OSZTV/Furdo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrandC_OSZTV
+{
+    public class Furdo
+    {
+        public string Nev { get; private set; }
+        public string Cim { get; private set; }
+        public int Ar { get; private set; }
+        public int VizHofok { get; private set; }
+
+        public Furdo(string nev, string cim, int ar, int vizHofok)
+        {
+            Nev = nev;
+            Cim = cim;
+            Ar = ar;
+            VizHofok = vizHofok;
+        }
+        public Furdo(string sor)
+        {
+            string[] temp=sor.Split(';');
+            Nev = temp[0];
+            Cim = temp[1];
+            Ar = int.Parse(temp[2]);
+            VizHofok = int.Parse(temp[3]);
+        }
+        public string IRSZ()
+        {
+            return Cim.Split(' ')[0];
+        }
+        public string Telepules()
+        {
+            return Cim.Split(' ')[1].TrimEnd(',');
+        }
+    }
+}
---
+++ StrandC_OSZTV/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+
+namespace StrandC_OSZTV
+{
+    internal class Program
+    {
+        static List<Furdo> furdok=new List<Furdo>();
+        static void Main(string[] args)
+        {
+            Feladat_5();
+            Feladat_7();
+            Feladat_8();
+            Feladat_9();
+            Feladat_10();
+        }
+
+        private static void Feladat_10()
+        {
+            Console.WriteLine("Kérem adja a fürdő nevét:");
+            string furdoNev=Console.ReadLine();
+
+            Furdo keresett=furdok.FirstOrDefault(f=>f.Nev==furdoNev);
+            if (keresett==null)
+            {
+                Console.WriteLine("Nincs ilyen nevű fürdő!");
+            }
+            else
+            {
+                Console.WriteLine($"A fürdő {keresett.Telepules()} településen van, irányítószáma: {keresett.IRSZ()}");
+            }
+
+        }
+
+        private static void Feladat_9()
+        {
+            string leghidegebb = furdok.FirstOrDefault(f => f.VizHofok == furdok.Min(x => x.VizHofok)).Nev;
+            Console.WriteLine($"9. feladat:\nA leghidegebb víz a(a) {leghidegebb} nevü fürdőben van.");
+        }
+
+        private static void Feladat_8()
+        {
+            double atlag = Math.Round(furdok.Average(f => f.Ar),1);
+            Console.WriteLine($"8. feladat\nA fürdőbelépők átlagos ára: {atlag}");
+        }
+
+        private static void Feladat_7()
+        {
+            Console.WriteLine($"7. feladat:\nFürdők száma: {furdok.Count}");
+        }
+
+        private static void Feladat_5()
+        {
+            string[] allomany=File.ReadAllLines("strandadatok.txt");
+            foreach (var item in allomany.Skip(1))
+            {
+                furdok.Add(new Furdo(item));
+            }
+        }
+    }
+}
